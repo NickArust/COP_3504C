05fa799d2b1f6de1455379ee25f4fe1f7ba0a328
NickArust <42815606+NickArust@users.noreply.github.com>
2021-10-13 13:59:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int linearSearch(vector<string> dataset, string element);
 int binarySearch(vector<string> dataset, string element);
 vector<long long> timerSearch (vector<string> dataset, string element);
 int main() {
-    vector<string> dataset = getStringData();
-    vector<long long> times = timerSearch(dataset, "aaaaa");
+    vector<string> dataset = getStringData(); // gets data
+    vector<long long> times = timerSearch(dataset, "aaaaa"); // does the timing for each value and prints result
     cout<<"aaaaa"<<endl;
     cout<< times[0] <<endl;
     cout<< times[1] <<endl;
@@ -23,7 +23,7 @@ int main() {
 
 int linearSearch(vector<string> dataset, string element) {
 
-    for(int i = 0;i<dataset.capacity();i++){
+    for(int i = 0;i<dataset.capacity();i++){ // does linear search
         if(dataset[i]==element){return i;}
     }
 
@@ -34,13 +34,13 @@ int binarySearch(vector<string> dataset, string element){
 
     int minIndex = 0;
     int maxIndex = dataset.capacity()-1;
-    int midIndex = minIndex + (maxIndex - minIndex) / 2;
+    int midIndex = minIndex + (maxIndex - minIndex) / 2; // sets the max, min, mid indexs
     while (minIndex <= maxIndex){
 
-        if(dataset[midIndex] == element){
+        if(dataset[midIndex] == element){ // returns if we have a match
             return midIndex;
         }
-        else if (element.compare(dataset[midIndex]) >0){
+        else if (element.compare(dataset[midIndex]) >0){ // changes indexs based on where we are reletive to element
             minIndex = midIndex+1;
 
         }
@@ -57,7 +57,7 @@ vector<long long> timerSearch (vector<string> dataset, string element){
     linearSearch(dataset, element);
     long long elTime = systemTimeNanoseconds();
     long long sbTime = systemTimeNanoseconds();
-    binarySearch(dataset, element);
+    binarySearch(dataset, element); // does the timing and returns array for binary and linear time
     long long ebTime = systemTimeNanoseconds();
     long long ltime = elTime-slTime;
     long long btime = ebTime- sbTime;
